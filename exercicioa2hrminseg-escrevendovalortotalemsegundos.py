194e9bd2ca80a3a18bccb57fec7a59fa682c1888
G3n1ls0nM3ss <gnewstudio@gmail.com>
2022-05-28 06:55:56 -0300

    Update and rename exercicioa2.py to exercicioa2hrminseg-escrevendovalortotalemsegundos.py

--- exercicioa2hrminseg-escrevendovalortotalemsegundos.py
+++ exercicioa2hrminseg-escrevendovalortotalemsegundos.py
@@ -1,4 +1,4 @@
-#coding:utf-8 (desmarquei porque meu teclado ta US aqui no linux)
+#coding:utf-8 
 # algoritimo que leia tempo, hr, min, seg escrevendo valor total em segundos
 # aluno Genilson
 # professor Habib
